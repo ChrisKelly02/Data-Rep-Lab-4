aaa43e6e7b3fb43691afb0c4dbad9cba38c7ee1e
ChrisKelly02 <G00395815@atu.ie>
2023-10-08 22:44:08 +0000

    lab 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header';
+import Content from './components/content';
+import Footer from './components/footer';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className="App"> 
+    <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="create">Create</Nav.Link>
+            <Nav.Link href="read">Read</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path='/Home' element={<Content></Content>}></Route>
+        <Route path='/Read' element={<Footer></Footer>}></Route>
+        <Route path= '/Create' element={<Header></Header>}></Route>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/header.js
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+        <div>
+            <h2>My header is another component</h2>
+        </div>
+    );
+}
+export default Header;
